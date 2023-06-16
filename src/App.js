beae41d8e0ec43cb4078bc743da9d44cc5e0bf55
tomcollins2110 <tom206collins@gmail.com>
2023-06-16 22:54:52 +0000

    'Added initial structure for index.js and app.js files'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-function App() {
+import ImageSlider from "./ImageSlider";
+const App = () => {
+  const slides = [
+    { url: "http://localhost:300/image-1.jpg", title:"Mountain"}, 
+    { url: "http://localhost:300/image-2.jpg", title:"Shop"},
+    { url: "http://localhost:300/image-3.jpg", title:"Table"},
+    { url: "http://localhost:300/image-4.jpg", title:"Dog"},
+    { url: "http://localhost:300/image-5.jpg", title:"Science"},
+  ];
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Carousel</h1>
+      <ImageSlider slides={slides}/>
     </div>
   );
 }
