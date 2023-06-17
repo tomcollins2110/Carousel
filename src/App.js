6b5b2a4f4c039823b0b92f8b5c5f021b840d48d2
tomcollins2110 <tom206collins@gmail.com>
2023-06-17 00:32:46 +0000

    Test commit for preview

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import ImageSlider from "./ImageSlider";
-const App = () => {
-  const slides = [
-    { url: "http://localhost:300/image-1.jpg", title:"Mountain"}, 
-    { url: "http://localhost:300/image-2.jpg", title:"Shop"},
-    { url: "http://localhost:300/image-3.jpg", title:"Table"},
-    { url: "http://localhost:300/image-4.jpg", title:"Dog"},
-    { url: "http://localhost:300/image-5.jpg", title:"Science"},
-  ];
+import "./App.css";
+
+function App() {
   return (
-    <div>
-      <h1>Carousel</h1>
-      <ImageSlider slides={slides}/>
+    <div className="App">
+    <header className="App-header">
+    <h1>Weather</h1>
+    </header>
     </div>
   );
 }
+
 
 export default App;
