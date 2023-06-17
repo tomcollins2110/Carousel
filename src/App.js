2af46e072193ead4085927230efb68d9ad9bc3f6
tomcollins2110 <tom206collins@gmail.com>
2023-06-17 01:43:11 +0000

    Linked api and wrote functions for search

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import "./App.css";
+import { useState } from "react";
 
 const api = {
   key: "2d5ee761bc6b8cc8e3a4e2d96188c7eb",
-  base: "https://api.openweathermap.org/data/2,5/",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App () {
+
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+    .then((res) => res.json())
+    .then((result) => {
+      setWeather(result);
+    });
+  };
+
   return (
     <div className="App">
     <header className="App-header">
    
     <h1>Weather</h1>
-   
-    <input type="text" placeholder="Search.." />
 
-    <p>Dublin</p>
+    <div>
+    <input type="text" placeholder="Search.." 
+    
+      onChange={(e) => setSearch(e.target.value)}
+    />
 
-    <p>32 C</p>
+    <button onClick={searchPressed}>Search</button>
+    </div>
+
+    <p>{weather.name}</p>
+
+    <p>{weather.main.temp}°C</p>
 
     <p>Misty</p>
 
