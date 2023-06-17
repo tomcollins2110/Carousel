9d6d2afaa12d4cc15e70759943709aa1861a22da
tomcollins2110 <tom206collins@gmail.com>
2023-06-17 00:53:42 +0000

    Re-added files as to link everything together

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 
-function App() {
+const api = {
+  key: "2d5ee761bc6b8cc8e3a4e2d96188c7eb",
+  base: "https://api.openweathermap.org/data/2,5/",
+};
+
+function App () {
   return (
     <div className="App">
     <header className="App-header">
+   
     <h1>Weather</h1>
+   
+    <input type="text" placeholder="Search.." />
+
+    <p>Dublin</p>
+
+    <p>32 C</p>
+
+    <p>Misty</p>
+
     </header>
     </div>
   );
